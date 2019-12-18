5c1bc27221d500727fcc7845e5c28bd301df11b1
vaibhavgupta <vaibhavg@hike.in>
2019-12-18 18:44:27 +0530

    Debugging (loss error)

--- models.py
+++ models.py
@@ -21,7 +21,7 @@ class AlexNet:
         self.idx_tensor = tf.Variable(np.array(self.idx_tensor, dtype=np.float32))
         self.dataset = dataset
         self.model = self.__create_model()
-        
+
     def __loss_angle(self, y_true, y_pred, alpha=0.5):
         # cross entropy loss
         bin_true = y_true[:,0]
@@ -36,7 +36,7 @@ class AlexNet:
 
     def __create_model(self):
         inputs = tf.keras.layers.Input(shape=(self.input_size, self.input_size, 3))
-        
+
         feature = tf.keras.layers.Conv2D(filters=64, kernel_size=(11, 11), strides=4, padding='same', activation=tf.nn.relu)(inputs)
         feature = tf.keras.layers.MaxPool2D(pool_size=(3, 3), strides=2)(feature)
         feature = tf.keras.layers.Conv2D(filters=192, kernel_size=(5, 5), padding='same', activation=tf.nn.relu)(feature)
@@ -48,27 +48,26 @@ class AlexNet:
         feature = tf.keras.layers.Flatten()(feature)
         feature = tf.keras.layers.Dropout(0.5)(feature)
         feature = tf.keras.layers.Dense(units=4096, activation=tf.nn.relu)(feature)
-        
+
         fc_yaw = tf.keras.layers.Dense(name='yaw', units=self.class_num)(feature)
         fc_pitch = tf.keras.layers.Dense(name='pitch', units=self.class_num)(feature)
         fc_roll = tf.keras.layers.Dense(name='roll', units=self.class_num)(feature)
-    
+
         model = tf.keras.Model(inputs=inputs, outputs=[fc_yaw, fc_pitch, fc_roll])
-        
+
         losses = {
             'yaw':self.__loss_angle,
             'pitch':self.__loss_angle,
             'roll':self.__loss_angle,
         }
-        
-        model.compile(optimizer=tf.train.AdamOptimizer(),
-                        loss=losses)
-       
+
+        model.compile(optimizer=tf.train.AdamOptimizer(), loss=losses)
+
         return model
 
     def train(self, model_path, max_epoches=EPOCHS, load_weight=True):
         self.model.summary()
-        
+
         if load_weight:
             self.model.load_weights(model_path)
         else:
@@ -80,7 +79,7 @@ class AlexNet:
                                     verbose=1)
 
             self.model.save(model_path)
-            
+
     def test(self, save_dir):
         for i, (images, [batch_yaw, batch_pitch, batch_roll], names) in enumerate(self.dataset.data_generator(test=True)):
             predictions = self.model.predict(images, batch_size=self.batch_size, verbose=1)
@@ -89,9 +88,9 @@ class AlexNet:
             pred_cont_pitch = tf.reduce_sum(tf.nn.softmax(predictions[1,:,:]) * self.idx_tensor, 1) * 3 - 99
             pred_cont_roll = tf.reduce_sum(tf.nn.softmax(predictions[2,:,:]) * self.idx_tensor, 1) * 3 - 99
             # print(pred_cont_yaw.shape)
-            
+
             self.dataset.save_test(names[0], save_dir, [pred_cont_yaw[0], pred_cont_pitch[0], pred_cont_roll[0]])
-            
+
     def test_online(self, face_imgs):
         batch_x = np.array(face_imgs, dtype=np.float32)
         predictions = self.model.predict(batch_x, batch_size=1, verbose=1)
@@ -100,6 +99,5 @@ class AlexNet:
         pred_cont_yaw = tf.reduce_sum(tf.nn.softmax(predictions[0, :, :]) * self.idx_tensor, 1) * 3 - 99
         pred_cont_pitch = tf.reduce_sum(tf.nn.softmax(predictions[1, :, :]) * self.idx_tensor, 1) * 3 - 99
         pred_cont_roll = tf.reduce_sum(tf.nn.softmax(predictions[2, :, :]) * self.idx_tensor, 1) * 3 - 99
-        
+
         return pred_cont_yaw[0], pred_cont_pitch[0], pred_cont_roll[0]
-        
