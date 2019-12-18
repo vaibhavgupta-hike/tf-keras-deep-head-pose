b32b0b49ca5a3d7fb32c2be041ee038769ebf0ba
vaibhavgupta <vaibhavg@hike.in>
2019-12-18 19:16:50 +0530

    Removed unused imports from head_pose

--- head_pose.py
+++ head_pose.py
@@ -1,8 +1,4 @@
-import tensorflow as tf
-import os
-import numpy as np
 import cv2
-import scipy.io as sio
 import dlib
 from imutils import face_utils
 
@@ -40,8 +36,8 @@ cap = cv2.VideoCapture(0)
 if not cap.isOpened():
     print("Unable to connect to camera.")
     exit(-1)
-    
-    
+
+
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(face_landmark_path)
 
@@ -55,17 +51,17 @@ while cap.isOpened():
             shape = face_utils.shape_to_np(shape)
 
             face_crop = utils.crop_face_loosely(shape, frame, INPUT_SIZE)
-            
+
             frames.append(face_crop)
             if len(frames) == 1:
                 print(shape[30])
                 pred_cont_yaw, pred_cont_pitch, pred_cont_roll = net.test_online(frames)
-                
+
                 cv2_img = utils.draw_axis(frame, pred_cont_yaw, pred_cont_pitch, pred_cont_roll, tdx=shape[30][0],
                                           tdy=shape[30][1], size=100)
                 cv2.imshow("cv2_img", cv2_img)
                 frames = []
-                
+
             # cv2.waitKey(0)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
